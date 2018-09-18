37113705f8c2f103ca63aa95f89de5c1a0512bf7
jordanraitt <jordanraitt@hotmail.com>
2018-09-18 20:19:59 +0100

    fish removed from un instanced river

--- specs/bears_specs.rb
+++ specs/bears_specs.rb
@@ -7,12 +7,12 @@ require_relative("../bears.rb")
 class BearsTest < MiniTest::Test
 
   def setup()
-    f1 = Fish.new("john")
-    f2 = Fish.new("lenny")
-    f3 = Fish.new("mark")
-    f4 = Fish.new("philip")
+    # f1 = Fish.new("john")
+    # f2 = Fish.new("lenny")
+    # f3 = Fish.new("mark")
+    # f4 = Fish.new("philip")
 
-    @river1 = River.new("Amazon", [f1, f2, f3, f4])
+    # @river1 = River.new("Amazon", [f1, f2, f3, f4])
 
     @bear1 = Bear.new("Martin")
   end
@@ -23,7 +23,7 @@ class BearsTest < MiniTest::Test
 
   def test_take_from_river()
     #maybe need to put something in here
-    @bear1.take_from_river(@river1.fish.last)
+    @bear1.take_from_river(@river)
     assert_equal(1, @bear1.stomach.count)
   end
 
